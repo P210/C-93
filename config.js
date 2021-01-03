8557e8759476f8f310275c25159761ad04a28604
P210 <53483555+P210@users.noreply.github.com>
2021-01-03 14:25:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyA89Wn1g27Ta5-zO5qFh6C7ZeH6JjybX6Y",
+    authDomain: "emergencyapp-bc065.firebaseapp.com",
+    projectId: "emergencyapp-bc065",
+    storageBucket: "emergencyapp-bc065.appspot.com",
+    messagingSenderId: "314002021651",
+    appId: "1:314002021651:web:c52d065383d92993dd4ee6"
+  };
+ 
